55a23a80f87331d8fd227d2e5a6340e527005143
soesandoval <115958384+soesandoval@users.noreply.github.com>
2023-03-27 19:56:38 -0500

    week 11 tasks

--- src/week11/MinimumValueUsingMap.java
+++ src/week11/MinimumValueUsingMap.java
@@ -1,11 +1,39 @@
 package week11;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MinimumValueUsingMap {
 
+    public static void minimumValueInMap(Map<String, Integer> map) {
+
+        Map.Entry<String, Integer> minNumber = null;
+
+        for (Map.Entry<String, Integer> number : map.entrySet()) {
+
+            if (minNumber == null || number.getValue().compareTo(minNumber.getValue()) < 0) {
+                minNumber = number;
+            }
+
+        }
+
+        System.out.println("Minimum number " + minNumber);
+
+    }
 
 
+    public static void main(String[] args) {
 
+
+        Map<String,Integer> map = new HashMap<>();
+
+        map.put("first", 30);
+        map.put("second", 20);
+        map.put("third", 10);
+
+        minimumValueInMap(map);
+
+    }
 
 }
